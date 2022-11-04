094ce4b38e505bd14f146d3e1fd22089129d7b3a
JoeyCranberry <carberry.joey@gmail.com>
2022-11-03 21:23:14 -0700

    Added Point amounts for different events

--- COD Zombies Remake/Assets/Scripts/Player/PlayerMoneyManager.cs
+++ COD Zombies Remake/Assets/Scripts/Player/PlayerMoneyManager.cs
@@ -10,4 +10,81 @@ public class PlayerMoneyManager : MonoBehaviour
     {
         playerManager = _playerManager;
     }
+
+    private int GetPerkPointCost(PerkMachineManager.PERK_MACHINE_TYPE type)
+    {
+        switch(type)
+        {
+            case PerkMachineManager.PERK_MACHINE_TYPE.JUGGERNOG:
+                return 10;
+            case PerkMachineManager.PERK_MACHINE_TYPE.SPEED_COLA:
+                return 10;
+            case PerkMachineManager.PERK_MACHINE_TYPE.DOUBLE_TAP:
+                return 10;
+            case PerkMachineManager.PERK_MACHINE_TYPE.QUICK_REVIVE:
+                return 10;
+            case PerkMachineManager.PERK_MACHINE_TYPE.PHD_FLOPPER:
+                return 10;
+            case PerkMachineManager.PERK_MACHINE_TYPE.STAMIN_UP:
+                return 10;
+            case PerkMachineManager.PERK_MACHINE_TYPE.DEADSHOT:
+                return 10;
+            case PerkMachineManager.PERK_MACHINE_TYPE.MULE_KICK:
+                return 10;
+            case PerkMachineManager.PERK_MACHINE_TYPE.TOMBSTONE:
+                return 10;
+            case PerkMachineManager.PERK_MACHINE_TYPE.WHOS_WHO:
+                return 10;
+            case PerkMachineManager.PERK_MACHINE_TYPE.ELECTRIC_CHERRY:
+                return 10;
+            case PerkMachineManager.PERK_MACHINE_TYPE.VULTURE_AID:
+                return 10;
+            case PerkMachineManager.PERK_MACHINE_TYPE.WIDOWS_WINE:
+                return 10;
+            default:
+                return 10;
+        }
+    }
+
+    private int GetPointAmount(POINT_TYPE type)
+    {
+        switch(type)
+        {
+            case POINT_TYPE.NON_LETHAL_HIT:
+                return 10;
+            case POINT_TYPE.LETHAL_TORSO_HIT:
+                return 60;
+            case POINT_TYPE.LETHAL_LIMB_HIT:
+            case POINT_TYPE.LETHAL_EXPLOSIVE_HIT:
+                return 50;
+            case POINT_TYPE.LETHAL_NECK_HIT:
+                return 70;
+            case POINT_TYPE.LETHAL_HEADSHOT:
+                return 100;
+            case POINT_TYPE.LETHAL_MELEE:
+                return 130;
+            case POINT_TYPE.PLANK_REPAIRED:
+                return 10;
+            case POINT_TYPE.COLLECT_CARPENTER:
+                return 200;
+            case POINT_TYPE.COLLECT_NUKE:
+                return 400;
+            default:
+                return 10;
+        }
+    }
+
+    public enum POINT_TYPE
+    {
+        NON_LETHAL_HIT,
+        LETHAL_TORSO_HIT,
+        LETHAL_LIMB_HIT,
+        LETHAL_EXPLOSIVE_HIT,
+        LETHAL_NECK_HIT,
+        LETHAL_HEADSHOT,
+        LETHAL_MELEE,
+        PLANK_REPAIRED,
+        COLLECT_CARPENTER,
+        COLLECT_NUKE
+    }
 }
